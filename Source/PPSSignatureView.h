02f23e7549f0ce1a2b7193ca68e392af26b4eb01
Julius Wilson Lundang <jwblundang@gmail.com>
2017-09-14 15:06:39 +0800

    :sparkles: Added signature delegate

--- Source/PPSSignatureView.h
+++ Source/PPSSignatureView.h
@@ -1,11 +1,22 @@
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
+@protocol PPSSignatureViewDelegate <NSObject>
+
+@optional
+- (void) didStartSigning;
+- (void) didEndSigning;
+- (void) didClearSignature;
+
+@end
+
 @interface PPSSignatureView : GLKView
 
 @property (assign, nonatomic) UIColor *strokeColor;
 @property (assign, nonatomic) BOOL hasSignature;
 @property (strong, nonatomic) UIImage *signatureImage;
+
+@property (weak, nonatomic) id<PPSSignatureViewDelegate> signatureViewDelegate;
 
 - (void)erase;
 
